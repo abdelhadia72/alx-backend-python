3c26aa3b3b78f8b715b824f499784d457af85ac7
abdelhadia72 <thisisavnb@gmail.com>
2024-04-18 17:49:08 +0100

    fix task 6 mixed array

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,11 @@
 from typing import List, Union
 
 
+"""
+    function return sum of list of floats
+"""
+
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
         function return sum of list of floats
