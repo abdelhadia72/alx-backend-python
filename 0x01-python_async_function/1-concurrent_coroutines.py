507b541b0451ab4e2345b2cd9659deee58522225
abdelhadia72 <thisisavnb@gmail.com>
2024-04-22 19:01:13 +0100

    loop n times on max_delay

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+from asyncio import gather
+wait_random = __import__('0-basic_async_syntax').wait_random
+"""
+Let's execute multiple coroutines at the same time with async
+"""
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ function take 2 args with max_delay with n times """
+    return await gather(*(wait_random(max_delay) for _ in range(n)))
