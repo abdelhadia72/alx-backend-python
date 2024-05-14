e41792bf390c6cd25cf9d2efcc6c2b3b5e366f60
abdelhadia72 <thisisavnb@gmail.com>
2024-05-14 16:11:54 +0100

    adding files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
+import unittest
+from parameterized import parameterized
 
-class TestAccessNestedMap:
-    def __init__(self):
-            
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+        
+    def test_access_nested_map(self):
+        pass
